3c0f2b8791e31031cc0fe3249b57ecc8651a8554
Angel Marte <robotking134@gmail.com>
2022-12-05 14:29:16 -0500

    commit tree a7f11ea7f64b51fb0fb8572d28e2c8c0d860a277

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
-  
+
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
   end
-  
+
   get '/bakeries/:id' do
     bakery = Bakery.find(params[:id])
     bakery.to_json(include: :baked_goods)
@@ -22,5 +22,24 @@ class ApplicationController < Sinatra::Base
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
+  post '/baked_goods' do
+    good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+     bakery_id: params[:bakery_id]
+    )
+    good.to_json
+  end
+  patch '/bakeries/:id' do
+    good = Bakery.find(params[:id])
+    good.update(name:params[:name])
+    good.to_json
+  end
+  delete '/baked_goods/:id' do
+    good = BakedGood.find(params[:id])
+    good.destroy
+    good.to_json
+  end
+
 
 end
